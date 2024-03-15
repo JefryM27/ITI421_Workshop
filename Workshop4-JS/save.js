e556a4e1b1622164520da70501dffac6c7990a4e
Jefry Morera <jefrymoreraespinoza@gmail.com>
2024-03-15 10:53:11 -0600

    Add the table for show the values of the localStorage

--- Workshop4-JS/save.js
+++ Workshop4-JS/save.js
@@ -1,36 +1,46 @@
 function guardar() {
-    event.preventDefault(); // Avoid that the form save
-
-    // get the values
+    event.preventDefault(); //This event is for avoiding to send
+    // get the values to form
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
     const telefono = document.getElementById('telefono').value;
-
-    // create the object with the date
+    // create a object
     const contacto = {
         nombre: nombre,
         apellido: apellido,
         telefono: telefono
     };
-
-    // get the dates into the imputs
+    // get the date
     const contactosGuardados = JSON.parse(localStorage.getItem('contactos')) || [];
-
-    // Add the content into the array
+    // add the new contact to array
     contactosGuardados.push(contacto);
-
-    // save the content in the JSON
+    // save the array to localStorage
     localStorage.setItem('contactos', JSON.stringify(contactosGuardados));
-
-    // Show the content in console
-    console.log("Arreglo de contactos guardados:", contactosGuardados);
-
     // Clean the form
     document.getElementById('formulario').reset();
-
-    //Option to show the message
+    // show the array in the table
+    mostrarContactos(contactosGuardados);
     alert('¡Contacto guardado correctamente!');
-    
 }
 
+// funtion to load the clients when I enter to the page
+window.onload = function() {
+    const contactosGuardados = JSON.parse(localStorage.getItem('contactos')) || [];
+    mostrarContactos(contactosGuardados);
+};
 
+function mostrarContactos(contactos) {
+    const tablaClientes = document.getElementById('tablaClientes');
+    tablaClientes.innerHTML = '';
+
+    contactos.forEach((contacto, index) => {
+        const fila = document.createElement('tr');
+        fila.innerHTML = `
+            <td>${index + 1}</td>
+            <td>${contacto.nombre}</td>
+            <td>${contacto.apellido}</td>
+            <td>${contacto.telefono}</td>
+        `;
+        tablaClientes.appendChild(fila);
+    });
+}
